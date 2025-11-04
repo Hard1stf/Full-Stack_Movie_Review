2f3145e76204412e5c73476e2d187c2d515fa449
Hard1stf <hardikvijeta2@gmail.com>
2025-11-04 22:01:31 +0530

    code: create review-controllers and export it.

---
+++ M-R_Backend-Server/api/reviews.controller.js
@@ -0,0 +1,89 @@
+import ReviewDAO from '../dao/reviewsDAO.js'; 1
+
+export default class ReviewsController {
+
+    // post controller logic: post/create new reviews
+    static async apiPostReview(req, res, next) {
+        try {
+            const movieId = req.body.movieId;
+            const review = req.body.review;
+            const user = req.body.user;
+
+            const reviewResponse = await ReviewDAO.addReview(
+                movieId,
+                user,
+                review
+            )
+            res.json({ status: 'success' });
+        } catch (error) {
+            res.status(500).json({ error: e.message });
+        }
+    }
+
+    // get controller logic: get the review based on the id
+    static async apiGetReview(req, res, next) {
+        try {
+            let id = req.params.id || {};
+            let review = await ReviewDAO.getReview(id);
+            if (!review) return res.status(404).json({ error: 'not found' });
+            res.json(review);
+        } catch (error) {
+            console.log(`api: ${error}`);
+            res.status(500).json({ error: error });
+        }
+    }
+
+    // put controller logic: update the existing review
+    static async apiUpdateReview(req, res, next) {
+        try {
+            const reviewId = req.params.id;
+            const review = req.body.review;
+            const user = req.body.user;
+
+            const reviewResponse = await ReviewDAO.updateReview(
+                reviewId,
+                user,
+                review
+            )
+
+            let { error } = reviewResponse;
+            if (error) {
+                res.status(400).json({ error });
+            }
+
+            if (reviewResponse.modifiedCount === 0) {
+                throw new Error('Unable to update review.');
+            }
+
+            res.jon({ status: 'success' })
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+    }
+
+    // delete controller logic: delete the review based on the reviewId
+    static async apiDeleteReview(req, res, next) {
+        try {
+            const reviewId = req.params.id;
+            const reviewResponse = await ReviewDAO.deleteReview(reviewId)
+            res.json({ status: 'success' });
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+    }
+
+    // get the list of all reviews controller logic: get all the reviews of the specific movie and there movieId
+    static async apiGetReviews(req, res, next){
+        try {
+            let id = req.params.id || {};
+            let reviews = await ReviewDAO.getReviewsByMovieId(id);
+
+            if(!reviews) return res.status(404).json({error: "Not found"});
+
+            res.json(reviews);
+        } catch (error) {
+            console.log(`api: ${error}`);
+            res.status(500).json({error: error});   
+        }
+    }
+}
