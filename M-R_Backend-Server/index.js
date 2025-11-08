b8621ecb19f917db2b64c46e1af4902eebbbe070
Hard1stf <hardikvijeta2@gmail.com>
2025-11-08 20:19:19 +0530

    fix(DB connect): update the MongoDB connection string.

--- M-R_Backend-Server/index.js
+++ M-R_Backend-Server/index.js
@@ -9,7 +9,7 @@ import ReviewsDAO from './dao/reviewsDAO.js';
 const MongoClient = mongodb.MongoClient;
 const mongo_username = process.env['MONGO_USERNAME'];
 const mongo_password = process.env['MONGO_PASSWORD'];
-const mongo_uri = `mongodb+srv://${mongo_username}:${mongo_password}@cluster0.bpoovu9.mongodb.net/`;
+const mongo_uri = `mongodb+srv://${mongo_username}:${mongo_password}@cluster-0-lp.h9ypnmz.mongodb.net/`;
 
 const port = 8000;
 MongoClient.connect(mongo_uri, {
